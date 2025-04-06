fa39c56c51c0224b8e1928e0cf5fc0d709e4663d
koustubh25 <kosta250@gmail.com>
2025-04-06 17:06:18 +1000

    trigger main

--- main.py
+++ main.py
@@ -76,3 +76,9 @@ async def generate(req: GenerateRequest):
     return JSONResponse(
         content={"message": "✅ Image generated", "output_dir": output_path}
     )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
